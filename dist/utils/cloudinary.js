e7dcfe92d66cfe89d10dc9306ca2efbb819d11fa
Manish <manish.dalal@moonshinetechnology.com>
2023-03-12 20:47:05 +0530

    skdjs

--- dist/utils/cloudinary.js
+++ dist/utils/cloudinary.js
@@ -5,13 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 // import { v2 as cloudinary } from "cloudinary";
-const cloudinary = {
-  config: () => ({
-    api_secret: "",
-    cloud_name: "",
-    api_key: ""
-  })
-};
+
 const config = require("../config");
 
 // Configure your cloud name, API key and API secret:
@@ -21,11 +15,15 @@ const {
   CLOUDINARY_API,
   CLOUDINARY_SECRET
 } = config;
-const myconfig = CLOUDINARY_NAME && CLOUDINARY_API && CLOUDINARY_SECRET ? cloudinary.config({
-  cloud_name: CLOUDINARY_NAME,
-  api_key: CLOUDINARY_API,
-  api_secret: CLOUDINARY_SECRET,
-  secure: true
-}) : {};
-var _default = myconfig;
+
+// const myconfig =
+//   CLOUDINARY_NAME && CLOUDINARY_API && CLOUDINARY_SECRET
+//     ? cloudinary.config({
+//         cloud_name: CLOUDINARY_NAME,
+//         api_key: CLOUDINARY_API,
+//         api_secret: CLOUDINARY_SECRET,
+//         secure: true
+//       })
+//     : {};
+var _default = {};
 exports.default = _default;
--- dist/utils/getCloudinarySignature.js
+++ dist/utils/getCloudinarySignature.js
@@ -6,28 +6,20 @@ Object.defineProperty(exports, "__esModule", {
 exports.getCloudinarySignature = void 0;
 require("./cloudinary");
 // import { v2 as cloudinary } from "cloudinary";
-const cloudinary = {
-  config: () => ({
-    api_secret: "",
-    cloud_name: "",
-    api_key: ""
-  })
-};
+
 const getCloudinarySignature = (folder = "m1") => {
-  const apiSecret = cloudinary.config().api_secret;
-  const cloudName = cloudinary.config().cloud_name;
-  const apiKey = cloudinary.config().api_key;
-  const timestamp = Math.round(new Date().getTime() / 1000);
-  const signature = cloudinary.utils.api_sign_request({
-    timestamp: timestamp,
-    folder
-  }, apiSecret);
-  return {
-    timestamp,
-    signature,
-    cloudname: cloudName,
-    apikey: apiKey,
-    folder
-  };
+  // const apiSecret = cloudinary.config().api_secret;
+  // const cloudName = cloudinary.config().cloud_name;
+  // const apiKey = cloudinary.config().api_key;
+  // const timestamp = Math.round(new Date().getTime() / 1000);
+  // const signature = cloudinary.utils.api_sign_request(
+  //   {
+  //     timestamp: timestamp,
+  //     folder
+  //   },
+  //   apiSecret
+  // );
+  // return { timestamp, signature, cloudname: cloudName, apikey: apiKey, folder };
+  return {};
 };
 exports.getCloudinarySignature = getCloudinarySignature;
